c157060b8700bd1ba98b8fb223aa266648f55355
Ulysse Herbach <35532736+ulysseherbach@users.noreply.github.com>
2025-04-08 15:07:45 +0200

    Small update

--- src/halva/__init__.py
+++ src/halva/__init__.py
@@ -1,17 +1,10 @@
-"""
-Halva
-=====
-
-Multivariate analysis of ordinal data with missing values and latent variables.
-"""
+"""Multivariate analysis of ordinal data with missing values."""
 from importlib.metadata import version as _version
 from halva.inference import infer_precision
 
-__all__ = [
-    "infer_precision",
-]
+__all__ = ['infer_precision']
 
 try:
-    __version__ = _version("halva")
+    __version__ = _version('halva')
 except Exception:
-    __version__ = "unknown version"
+    __version__ = 'unknown version'
